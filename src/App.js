d0851e55a4b83cca1ae496fcd63c5d4ec1022f7d
saisandeepd7 <dsandeep3690@gmail.com>
2022-06-03 16:00:25 +0530

    Message list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { getMouseEventOptions } from '@testing-library/user-event/dist/utils';
+export default function App(){
+const students=[
+  {
+    name:"mayuri",
+    pic:"https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"
+  },
+  {
+    name:"gagan",
+    pic:""
+  },
+  {
+    name:"prerana",
+    pic:""
+  }
+]
+return(
+  <div className='App'>
+    {students.map((students)=>(<Message name={students.name} pic={students.pic}/>))}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  </div>
+)
 
-export default App;
+function Message(props){
+
+return(
+  <div>
+   <img
+   className='profile-pic'
+   src={props.pic} alt={props.name} />
+    
+  <h1>hello {props.name} 🎉🎉🤩</h1>
+  </div>
+)
+}}
